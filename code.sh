0a50b35a7f36f686288fa9f72b4be4a8f4e96530
THIAGO SANTOS DE SOUZA <75391184+thiago031996@users.noreply.github.com>
2024-06-18 18:36:07 -0300

    Create code.sh
    Salve esse arquivo com a extensão .sh, em seguida dê permissão de execução. Pronto, basta executar o arquivo usando.\code.sh

---
+++ code.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#Este script realiza o teste da resolução de um DNS especificado para um host especificado
+#
+
+owner_project="Thiago Souza"
+
+#CABEÇALHO DE INFORMAÇÕES
+echo -e "========================================================================="
+
+echo -e "Esse script realiza a consulta ao server DNS para resolução de nomes"
+echo -e "Finalidade: Análise de DNS para resolução de nomes"
+echo -e "Owner_script: Thiago Souza"
+echo -e "Para mais informações acesse o meu github:https://github.com/thiago031996"
+
+echo -e "=========================================================================="
+#DADOS INSERIDOS PELO USUÁRIO
+echo "Informe o DNS que será usado para a resolução: "
+read dns
+printf "\n"
+
+echo "Informe o IP / Dominio para consulta: "
+read ip_domain
+printf "\n"
+
+#(dig)ANÁLISE DNS PARA O DOMÍNEO COM OS FILTROS APLICADOS
+echo -e "==============================================================="
+
+dig +time=1 +tries=2 @$dns $ip_domain | grep NOERROR && echo O DNS:"$dns" Resolveu o Endereço:"$ip_domain" com sucesso! || echo o DNS: "$dns" não conseguiu resolver o Endereço:"$ip_domain"
+
+echo -e "==============================================================="
+
+printf "\n"
+
+echo "INFORMAÇÕES DNS:$dns"
+
+inf_dns=$(whois -h whois.cymru.com " -v $dns")
+
+printf "\n"
+
+echo "$inf_dns" 
+
+#CONSULTA REVER
+for ip in "${dns[@]}"; do
+  echo "Consultando reverso para IP: $ip"
+  nome_reverso=$(dig +short -x "$ip")
+  if [ -n "$nome_reverso" ]; then
+    echo "Nome de domínio: $nome_reverso"
+  else
+    echo "Não foi possível resolver o IP: $ip"
+  fi
+  echo ""
+done
+
+#TESTE PING PARA O DNS
+ping=$(ping -c 4 -W 1 $dns)
+echo " Testando ping..."
+echo " 4 pacotes enviados para:$dns "
+
+result_ping=$(echo "$ping"| tail -n3)
+
+echo "Dados teste PING:$result_ping"
